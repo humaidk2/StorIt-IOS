eb9573b310caabf4c33c6e8f8be450f9467075ea
Humaid Khan <humaidk2@gmail.com>
2019-12-24 00:07:02 +0400

    Initial Commit

---
+++ StorIt-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StorIt-IOS
+//
+//  Created by Amad Khan on 24/12/2019.
+//  Copyright © 2019 Cyber-Monkeys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
