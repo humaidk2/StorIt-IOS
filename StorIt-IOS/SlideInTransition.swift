950b2a5fe828f00f5ffb8ae9b503c43d61f0abc8
Fidel Lim <lim.fidel@gmail.com>
2019-12-25 16:28:24 +0400

    add animation for nav drawer

---
+++ StorIt-IOS/SlideInTransition.swift
@@ -0,0 +1,70 @@
+//
+//  SlideInTransition.swift
+//  StorIt-IOS
+//
+//  Created by Fidel Lim on 12/25/19.
+//  Copyright © 2019 Cyber-Monkeys. All rights reserved.
+//
+
+import UIKit
+
+class SlideInTransition: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    var isPresenting = false
+    let dimmingView = UIView()
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 1.0
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let toViewController = transitionContext.viewController(forKey: .to),
+            let fromViewController = transitionContext.viewController(forKey: .from) else {
+                return
+        }
+        
+        let containerView = transitionContext.containerView
+        
+        let finalWidth = toViewController.view.bounds.width * 0.8
+        let finalHeight = toViewController.view.bounds.height
+        
+        if isPresenting {
+            //Add dimming View
+            dimmingView.backgroundColor = .lightGray
+            dimmingView.alpha = 0.0
+            containerView.addSubview(dimmingView)
+            dimmingView.frame = containerView.bounds
+            
+            //add menu view controller to container
+            containerView.addSubview(toViewController.view)
+            
+            //init frame off the screen
+            toViewController.view.frame = CGRect(x: -finalWidth, y: 0, width: finalWidth, height: finalHeight)
+        }
+        
+        //Animate on screen
+        let transform = {
+            self.dimmingView.alpha = 0.5
+            toViewController.view.transform = CGAffineTransform(translationX: finalWidth, y: 0)
+        }
+        
+        //Animate back off screen
+        let identity = {
+            self.dimmingView.alpha = 0.0
+            fromViewController.view.transform = .identity
+        }
+        
+        //Animation of the transition
+        let duration = transitionDuration(using: transitionContext)
+        let isCancelled = transitionContext.transitionWasCancelled
+        UIView.animate(withDuration: duration, animations: {
+            self.isPresenting ? transform() : identity()
+        }) { (_) in
+            transitionContext.completeTransition(!isCancelled)
+        }
+        
+    }
+    
+
+    
+}
