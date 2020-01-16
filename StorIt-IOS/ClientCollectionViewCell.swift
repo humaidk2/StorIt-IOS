707b4b54872975adaa7071abd2f3c4d7619e9f23
Fidel Lim <lim.fidel@gmail.com>
2020-01-17 03:26:05 +0400

    add collection view cell for client page

---
+++ StorIt-IOS/ClientCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ClientCollectionViewCell.swift
+//  StorIt-IOS
+//
+//  Created by Fidel Lim on 1/17/20.
+//  Copyright © 2020 Cyber-Monkeys. All rights reserved.
+//
+
+import UIKit
+
+class ClientCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var fileType: UIImageView!
+    @IBOutlet weak var fileName: UILabel!
+    
+}
