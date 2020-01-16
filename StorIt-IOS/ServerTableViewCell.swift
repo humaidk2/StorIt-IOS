eb6ac36391cb856e97a8a99212a28b663ee6b299
Fidel Lim <lim.fidel@gmail.com>
2020-01-16 05:01:16 +0400

    add custom cell for server page

---
+++ StorIt-IOS/ServerTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ServerTableViewCell.swift
+//  StorIt-IOS
+//
+//  Created by Fidel Lim on 1/16/20.
+//  Copyright © 2020 Cyber-Monkeys. All rights reserved.
+//
+
+import UIKit
+
+class ServerTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var serverName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
