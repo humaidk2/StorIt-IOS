05e9cf8089d6c3c3fb383fe4df4b0da513f9080b
Fidel Lim <lim.fidel@gmail.com>
2020-01-16 05:07:01 +0400

    add help page

---
+++ StorIt-IOS/HelpViewController.swift
@@ -0,0 +1,49 @@
+//
+//  HelpViewController.swift
+//  StorIt-IOS
+//
+//  Created by Fidel Lim on 1/15/20.
+//  Copyright © 2020 Cyber-Monkeys. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //for left bar button
+        let backButton = UIBarButtonItem(image: UIImage(named: "back-24"), style: .plain, target: self, action: #selector(goBack))
+        
+        self.navigationItem.leftBarButtonItem = backButton
+       
+    }
+    
+    //Go back to Menu
+    @objc func goBack(){
+         dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+//for navigation controller
+class HelpNavigationController : UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
