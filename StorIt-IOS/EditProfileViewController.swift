fc22e9a2868489a6ea2b6e485dd442d53ced0787
Fidel Lim <lim.fidel@gmail.com>
2020-01-23 19:19:04 +0400

    add edit profile view controller

---
+++ StorIt-IOS/EditProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditProfileViewController.swift
+//  StorIt-IOS
+//
+//  Created by Fidel Lim on 1/23/20.
+//  Copyright © 2020 Cyber-Monkeys. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
