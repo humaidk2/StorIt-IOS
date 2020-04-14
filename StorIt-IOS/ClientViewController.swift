4611199c1472cff3d764f01786f88ec821a74fdb
Fidel Lim <lim.fidel@gmail.com>
2020-04-14 23:23:10 +0400

    update loadCurrentDirectory function

--- StorIt-IOS/ClientViewController.swift
+++ StorIt-IOS/ClientViewController.swift
@@ -385,6 +385,17 @@ class ClientViewController: UIViewController, UICollectionViewDelegate, UICollec
                 self.loadDirectory()
             } else {
                 print("Document doesn't exist")
+                let dataToSave: [String : Any] = [
+                    "dir" : ","
+                ]
+                self.db.document(self.documentPath).setData(dataToSave)
+                { err in
+                    if let err = err {
+                        print("Error writing document: \(err)")
+                    }else{
+                        print("Document successfully written!")
+                    }
+                }
             }
         }
     }
