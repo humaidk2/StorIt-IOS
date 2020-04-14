d6a340759f1911ef5fc11aa6e905d473d11daa80
Humaid Khan <humaidk2@gmail.com>
2020-04-15 01:17:51 +0400

    Add File class

--- StorIt-IOS/File.swift
+++ StorIt-IOS/File.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+
+class File {
+    private var fileId:Int = 0
+    private var fileSize:Int = 0
+    private var fileName:String = ""
+    private var fileType:String = ""
+    private var children:Array<File> = []
+    private var fileImageName:String = "background_2"
+    
+    init(fileId:Int, fileSize:Int, fileName:String, fileType:String, isFolder:Bool) {
+        self.fileId = fileId
+        self.fileSize = fileSize
+        self.fileName = fileName
+        self.fileType = fileType
+        if(!isFolder) {
+            fileImageName = "background_2"
+        } else {
+            fileImageName = "background_3"
+        }
+    }
+    init() {
+        
+    }
+    
+    func getFileId() -> Int { return fileId }
+    
+    func getFileSize() -> Int { return fileSize }
+    
+    func getFileName() -> String { return fileName }
+    
+    func getFileType() -> String { return fileType }
+    
+    func getFileImageName() -> String { return fileImageName }
+    
+    func setFileId(fileId:Int) { self.fileId = fileId }
+    
+    func setFileSize(fileSize:Int) { self.fileSize = fileSize }
+    
+    func setFileName(fileName:String) { self.fileName = fileName }
+    
+    func setFileType(fileType:String) { self.fileType = fileType }
+    
+    func setFileImageName(fileImageName:String) { self.fileImageName = fileImageName }
+    
+}
